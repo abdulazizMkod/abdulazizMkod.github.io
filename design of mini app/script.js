7f77ea0539324440e5e95cffd0915d823de0ba9c
abdulazizMkod <132377513+abdulazizMkod@users.noreply.github.com>
2024-09-20 13:29:21 +0500

    Delete design of mini app directory
    Signed-off-by: abdulazizMkod <132377513+abdulazizMkod@users.noreply.github.com>

--- design of mini app/script.js
+++
@@ -1,5 +0,0 @@
-const planets = document.querySelectorAll('.planet');
-planets.forEach((planet, index) => {
-    planet.style.animationDuration = `${5 + index * 2}s`;
-    planet.style.transform = `rotate(${index * 120}deg)`;
-});
